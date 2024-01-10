b3a7b84d35237c0229bec813b83c90ff0b0787d5
Jaycee-9 <147878964+Jaycee-9@users.noreply.github.com>
2024-01-10 20:07:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createWriteStream } from "fs";
+import morgan from "morgan";
+import session from "express-session";
+import compression from "compression";
+import home from "./routes/home/index.js";
+
+import api from "./routes/api/index.js";
+import connectToDb from "./db/index.js";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const logFile = join(__dirname, "gram.log");
+const PORT = 1234;
+
+app.use(compression());
+app.use("/assets", express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, "public", "client")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  "/admin",
+  session({
+    name: "sessId",
+    secret: process.env.sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: app.get("env") === "production" ? true : false,
+      httpOnly: true,
+      maxAge: 18000000, // 5 hours
+    },
+  })
+);
+app.use(morgan(":method - :url - :date - :response-time ms"));
+app.use(
+  morgan(":method - :url - :date - :response-time ms", {
+    stream: createWriteStream(logFile, { flags: "a" }),
+  })
+);
+
+app.set("view engine", "pug");
+
+app.use("/api", api);
+app.use("/", home);
+
+Promise.all([connectToDb()])
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => {
+    console.error(`MongoDB Atlas Error: ${error}`);
+    process.exit();
+  });
